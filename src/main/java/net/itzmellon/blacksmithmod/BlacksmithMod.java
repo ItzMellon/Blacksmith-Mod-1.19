2a10002e90f434366ebead831ee2d13f306e0523
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 11:44:25 -0400

    Initial commit

---
+++ src/main/java/net/itzmellon/blacksmithmod/BlacksmithMod.java
@@ -0,0 +1,15 @@
+package net.itzmellon.blacksmithmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlacksmithMod implements ModInitializer {
+	public static final String MOD_ID = "blacksmithmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/itzmellon/blacksmithmod/BlacksmithModClient.java
@@ -0,0 +1,10 @@
+package net.itzmellon.blacksmithmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlacksmithModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
